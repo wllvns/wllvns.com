6f891c20417edf96913484df3bafff8c50762411
Will Evans <wevans@ironsidegroup.com>
2017-10-24 12:25:10 -0400

    Update home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,6 +2,6 @@ import React from 'react';
 import './index..css';
 
 const Home = (className) =>
-	<div className="pages-paragraph">Welcome to my site (which as you can see is currently under development). On these pages you'll find some of my work and hobbies across a variety of disciplines, and some other things I just find interesting. Unless a link to an external blog, everything on this site was developed created, shot, filmed, and/or edited by me. This site is developed in React then published via a Bash script that runs Python to update the media location and tags before syncing with S3 so the site is available to you. Please take a look around and feel free to contact me at the info in the footer with questions or comments on anything.</div>
+	<div className="pages-paragraph">Welcome to my site (which as you can see is currently under development). On these pages you'll find some of my work and hobbies across a variety of disciplines, and some other things I just find interesting. Unless a link to an external blog, everything on this site was developed created, shot, filmed, and/or edited by me. This site is developed in React, media locations are updated via Python scripts, then it's published out to S3. Please take a look around and feel free to contact me at the info in the footer with questions or comments on anything.</div>
 
 export default Home;
